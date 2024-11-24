76fdb3ea632fb2e912d05d039f44d29acde2004a
saikiranmvss <saikiran.m.v.s.s@gmail.com>
2024-11-24 12:51:16 +0530

    load

--- app.js
+++ app.js
@@ -8,29 +8,32 @@ const apiRouter = require('./routes/index');
 const path = require('path');
 const session = require('express-session');
 
-// Set up session middleware **before** routes
 app.use(session({
     secret: 'your_secret_key',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Set secure: true if using HTTPS
+    cookie: { secure: false } 
 }));
 
-// View engine and static file setup
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Middleware setup
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(routerMiddleware);
 
-// Define routes
+
 app.use('/', apiRouter);
 
-// Start the server
+app.use((err, req, res, next) => {
+    console.error(err.stack); 
+    res.status(500).render('500', { error: err }); 
+});
+
+
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
