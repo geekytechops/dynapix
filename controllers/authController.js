a22eab5e386743c3f52af6a293d337f0b30e0b43
saikiranmvss <saikiran.m.v.s.s@gmail.com>
2024-11-27 21:37:28 +0530

    loadeddd

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const UserModel = require('../models/userModel');
+const UserModel = require('../models/UserModel');
 const session = require('express-session');
 
 exports.login = async (req, res) => {
